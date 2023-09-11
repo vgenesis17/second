d750c3d9baadf660428eea16d162bdb5606148a6
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-09-11 08:53:55 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import mymodel as m
 st.write("""#First App *world!*""")
-windows = st.slider("Forcast Prediction (days)")
+window = st.slider("Forcast Prediction (days)")
 st.write(m.run(window=window))
 
