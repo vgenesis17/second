68c8b24d9843f989502887b68b5dfa98f38cc759
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-09-14 11:11:23 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import plotly.express as px
+import pandas as pd
+import os
 st.title(':bar_chart: Super Store') 
 st.text_input('Search')
 
@@ -8,7 +11,8 @@ if fl is not None:
      st.write(filename)
      df=pd.read_csv(filename, encoding = "ISO-8859-1")
 else:
-    df = pd.read_csv("Superstore.csv", encoding = "ISO-8859-1")
+     os.chdir(r"C:\Users\genesis.villagracia\Downloads")
+    df = pd.read_csv("Sample - Superstore.xlsx", encoding = "ISO-8859-1")
 
 
 
