4ffa6307de6b30e455f201418a180249f74aaca7
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-09-14 10:49:40 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title('Super Store') 
+st.title(':bar_chart: Super Store') 
 st.text_input('Search')
 
 st.set_page_config(page_title="Superstore!",page_icon=":bar_chart:",layout="wide")
