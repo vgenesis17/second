b3726f7cdef50f33f0b0bdcba07e406614fa24ff
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-09-14 11:14:43 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -9,7 +9,7 @@ fl = st.file_uploader(":file_folder: Upload a file",type(["csv","txt","xlsx","xl
 if fl is not None:
      filename=fl.name
      st.write(filename)
-     df=pd.read_csv(filename, encoding = "ISO-8859-1")
+     df=pd.read_xlsx(filename, encoding = "ISO-8859-1")
 else:
      os.chdir(r"C:\Users\genesis.villagracia\Downloads")
     df = pd.read_xlsx("Sample - Superstore.xlsx", encoding = "ISO-8859-1")
