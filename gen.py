f569976393f1100604de118017dd90dcf377909d
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-09-14 11:15:29 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -6,7 +6,7 @@ st.title(':bar_chart: Super Store')
 st.text_input('Search')
 
 fl = st.file_uploader(":file_folder: Upload a file",type(["csv","txt","xlsx","xls"])
-if fl is not None:
+if fl is None:
      filename=fl.name
      st.write(filename)
      df=pd.read_xlsx(filename, encoding = "ISO-8859-1")
