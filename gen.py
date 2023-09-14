1b3bfae853f1c21e22befd0cf253dc4d0eb48024
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-09-14 14:37:10 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -11,12 +11,12 @@ st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allo
 
 fl = st.file_uploader(":file_folder: Upload a file",type(["csv","txt","xlsx","xls"]))
 if fl is not None:
-     filename = fl.name
-     st.write(filename)
-     df = pd.read_csv(filename, encoding = "ISO-8859-1")
+                  filename = fl.name
+                 st.write(filename)
+                  df = pd.read_csv(filename, encoding = "ISO-8859-1")
 else:
-     os.chdir(r"C:\Users\genesis.villagracia\Downloads")
-    df = pd.read_csv("Sample - Superstore.csv", encoding = "ISO-8859-1")
+        os.chdir(r"C:\Users\genesis.villagracia\Downloads")
+        df = pd.read_csv("Sample - Superstore.csv", encoding = "ISO-8859-1")
 
 
 
