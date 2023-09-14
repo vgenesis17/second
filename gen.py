8dfa9b93ea4e72797e6ff5b496a0097f643a4bab
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-09-14 11:21:36 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -2,8 +2,12 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 import os
+import warnings
+warnings.filterwarnings('ignore')
+
+
 st.title(':bar_chart: Super Store') 
-st.text_input('Search')
+st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
 
 fl = st.file_uploader(":file_folder: Upload a file",type(["csv","txt","xlsx","xls"])
 if fl is None:
