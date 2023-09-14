b4bc6633aa7900060ffc493d7b49cb6a51be71f6
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-09-14 14:46:02 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -9,14 +9,6 @@ warnings.filterwarnings('ignore')
 st.title(':bar_chart: Super Store') 
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
 
-fl = st.file_uploader(":file_folder: Upload a file",type(["csv","txt","xlsx","xls"]))
-if fl is not None:
-                  filename = fl.name
-                  st.write(filename)
-                  df = pd.read_csv(filename, encoding = "ISO-8859-1")
-else:
-        os.chdir(r"C:\Users\genesis.villagracia\Downloads")
-        df = pd.read_csv("Sample - Superstore.csv", encoding = "ISO-8859-1")
 
 
 
