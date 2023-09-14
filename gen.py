866602a3ae74497d9fb496848147a9d352a8a415
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-09-14 11:02:42 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -2,6 +2,13 @@ import streamlit as st
 st.title(':bar_chart: Super Store') 
 st.text_input('Search')
 
+fl = st.file_uploader(":file_folder: Upload a file",type(["csv","txt","xlsx","xls"])
+if fl is not None:
+     filename=fl.name
+     st.write(filename)
+     df=pd.read_csv(filename, encoding = "ISO-8859-1")
+else:
+    df = pd.read_csv("Superstore.csv", encoding = "ISO-8859-1")
 
 
 
