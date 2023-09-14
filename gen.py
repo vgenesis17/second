e307d7c2f97e26c06c900b3242498904f35680d0
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-09-14 10:10:50 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,6 +1,5 @@
 import streamlit 
 streamlit.title('Filipino Comicz') 
-st.text_input('Search')
 streamlit.header('Lastik Man (Ruber man)')
 streamlit.text('A Filipino superhero that has the capability of a ruber.')
 streamlit.text('He can stretch like a ruber, bounce like a ruber and transform')
