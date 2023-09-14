33f83ee99efc6a65a8f531753263a440bc3cf2d3
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-09-14 14:54:54 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+import pandas as pd
+import os 
 
 st.title(':bar_chart: Super Store') 
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
