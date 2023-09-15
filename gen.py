a093be90e50a30aa16e2916a384a74c553b93276
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-09-15 09:01:15 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -5,8 +5,6 @@ import warnings
 
 # Ignore warnings
 warnings.filterwarnings("ignore")
-=======
->>>>>>> parent of 025849e (Update gen.py)
 
 st.title(':bar_chart: Super Store') 
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
