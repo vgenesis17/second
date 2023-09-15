c6e6d9278b7997437cf7d18d7f5a058e54404462
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-09-15 08:28:31 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -10,16 +10,19 @@ st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allo
 
 fl = st.file_uploader(":file_folder: Upload a file", type=(["csv","txt","xlsx","xls"]))
 
-#Defines Columns  
-col1,col2 = st.columns ((2))
-df["Order_Date"] = pd.to_datetime(df["Order Date"])
-#getting max and min date in this column
-startDate = pd.to_datetime(df["Order Date"]).min()
-endDate = pd.to_datetime(df["Order Date"]).max()
-#DisplayToColumns
+# Assuming you have a DataFrame named df
+col1, col2 = st.columns(2)
 
-with col1:
-     date1 = pd.to_datetime(st.date_input("Start Date", startDate))
-with col2:
-     date2 = pd.to_datetime(st.date_input("End Date", endDate))
-df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
+# Check if the DataFrame has an "Order Date" column
+if "Order Date" in df.columns:
+    df["Order_Date"] = pd.to_datetime(df["Order Date"])
+    
+    # Getting the min and max date in the "Order_Date" column
+    startDate = df["Order_Date"].min()
+    endDate = df["Order_Date"].max()
+    
+    # Display the min and max dates
+    col1.write(f"Start Date: {startDate}")
+    col2.write(f"End Date: {endDate}")
+else:
+    st.write("The 'Order Date' column does not exist in the DataFrame.")
