e5b8b736777b97d8770cd6b127fbe70370a89c03
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-09-15 14:34:04 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,45 +1,13 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import os 
+import warnings
 
-# Load data from a CSV file (place your data in the 'data/' directory)
-@st.cache  # Cache the loaded data to improve performance
-def load_data():
-    data = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")  # Adjust the file name as needed
-    return data
+# Ignore warnings
+warnings.filterwarnings("ignore")
 
-# Create a Streamlit app
-def main():
-    st.title("Sample Streamlit Data Visualization")
-    st.sidebar.header("Settings")
+st.title(':bar_chart: Super Store') 
+st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
 
-    # Load the data
-    data = load_data()
-
-    # Sidebar options for data manipulation
-    st.sidebar.subheader("Data Manipulation")
-    selected_column = st.sidebar.selectbox("Select a column:", data.columns)
-    
-    # Filter the data based on user selection
-    filtered_data = data[data[selected_column] > st.sidebar.slider(f"Filter by {selected_column}", min_value=0, max_value=data[selected_column].max())]
-
-    # Display the filtered data
-    st.subheader("Filtered Data")
-    st.write(filtered_data)
-
-    # Data visualization
-    st.subheader("Data Visualization")
-    chart_type = st.selectbox("Select a chart type:", ["Bar Chart", "Scatter Plot"])
-    
-    if chart_type == "Bar Chart":
-        st.write("### Bar Chart")
-        fig = px.bar(data, x="Category", y="Sales", title="Sales by Category")
-        st.plotly_chart(fig)
-
-    elif chart_type == "Scatter Plot":
-        st.write("### Scatter Plot")
-        fig = px.scatter(data, x="Profit", y="Sales", color="Category", title="Profit vs. Sales")
-        st.plotly_chart(fig)
-
-if __name__ == "__main__":
-    main()
+# File upload
+fl = st.file_uploader(":file_folder: Upload a file", type=(["csv","txt","xlsx","xls"]))
