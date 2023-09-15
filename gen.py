e175071da9064cefddaaf8dec8e73c4f8d54645f
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-09-15 09:38:01 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -12,32 +12,32 @@ st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allo
 # File upload
 fl = st.file_uploader(":file_folder: Upload a file", type=(["csv","txt","xlsx","xls"]))
 
-#if fl is not None:
-   # filename = fl.name
-    #st.write(filename)
-   # st.dataframe = pd.read_xlsx(filename)
-#else:
-   # os.chdir(r"C:\Users\genesis.villagracia\Downloads")
-    #st.dataframe = pd.read_xlsx("Sample - Superstore.xlsx")
+if fl is not None:
+   filename = fl.name
+    st.write(filename)
+   st.dataframe = pd.read_xlsx(filename)
+else:
+   os.chdir(r"C:\Users\genesis.villagracia\Downloads")
+   st.dataframe = pd.read_xlsx("Sample - Superstore.xlsx")
 
 
 #Defines Columns  
-col1,col2 = st.columns (2)
-st.dataframe["Order_Date"] = pd.to_datetime(st.dataframe["Order Date"])
+#col1,col2 = st.columns (2)
+#st.dataframe["Order_Date"] = pd.to_datetime(st.dataframe["Order Date"])
 
 
 
 #getting max and min date in this column
-startDate = pd.to_datetime(st.dataframe["Order Date"]).min()
-endDate = pd.to_datetime(st.dataframe["Order Date"]).max()
+#startDate = pd.to_datetime(st.dataframe["Order Date"]).min()
+#endDate = pd.to_datetime(st.dataframe["Order Date"]).max()
 #DisplayToColumns
 
-with col1:
-     date1 = pd.to_datetime(st.date_input("Start Date", startDate))
-with col2:
-     date2 = pd.to_datetime(st.date_input("End Date", endDate))
+#with col1:
+     #date1 = pd.to_datetime(st.date_input("Start Date", startDate))
+#with col2:
+     #date2 = pd.to_datetime(st.date_input("End Date", endDate))
     
-st.dataframe = st.dataframe[(st.dataframe["Order Date"] >= date1) & (st.dataframe["Order Date"] <= date2)].copy()
+#st.dataframe = st.dataframe[(st.dataframe["Order Date"] >= date1) & (st.dataframe["Order Date"] <= date2)].copy()
 
 
 
