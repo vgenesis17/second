013f184f4a7a7986aadac6800698d8b66afaf0c6
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-09-15 14:51:38 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -11,5 +11,29 @@ st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allo
 
 # File upload
 fl = st.file_uploader(":file_folder: Upload a file", type=(["csv","txt","xlsx","xls"]))
+st.dataframe(fl)
+
+
+
+
+
+
+#Defines Columns  
+#col1,col2 = st.columns (2)
+#st.df["Order_Date"] = pd.to_datetime(df["Order Date"])
+
+
+
+#getting max and min date in this column
+#startDate = pd.to_datetime(df["Order Date"]).min()
+#endDate = pd.to_datetime(df["Order Date"]).max()
+#DisplayToColumns
+
+#with col1:
+     #date1 = pd.to_datetime(st.date_input("Start Date", startDate))
+#with col2:
+     #date2 = pd.to_datetime(st.date_input("End Date", endDate))
+    
+#df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
 
 
