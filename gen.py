8888d9ec5cb079ffbd89c106ba9dcbf02b0c5ae9
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-09-15 15:21:18 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -12,7 +12,10 @@ st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allo
 
 # File upload
 fl = st.file_uploader(":file_folder: Upload a file", type=(["csv","txt","xlsx","xls"]))
-fl = fl.set_index('plant_name')
+
+
+fls = pandas.read_csv(fl)
+fls = fl.set_index('plant_name')
 #Display table
 #st.dataframe(fl)
 
