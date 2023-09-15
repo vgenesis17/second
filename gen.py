a845c4074a7a639c9b75577d6051217030836fe4
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-09-15 09:17:51 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -19,3 +19,29 @@ if fl is not None:
 else:
     os.chdir(r"C:\Users\genesis.villagracia\Downloads")
     df = pd.read_xlsx("Sample - Superstore.xlsx")
+
+
+#Defines Columns  
+col1,col2 = st.columns ((2))
+df["Order_Date"] = pd.to_datetime(df["Order Date"])
+
+
+
+#getting max and min date in this column
+startDate = pd.to_datetime(df["Order Date"]).min()
+endDate = pd.to_datetime(df["Order Date"]).max()
+#DisplayToColumns
+
+with col1:
+     date1 = pd.to_datetime(st.date_input("Start Date", startDate))
+with col2:
+     date2 = pd.to_datetime(st.date_input("End Date", endDate))
+    
+df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
+
+
+
+
+
+
+
